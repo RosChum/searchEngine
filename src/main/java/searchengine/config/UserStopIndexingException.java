dd55e8fe6c0fa55333c23a31eec1971f5d61f756
RosChum <rostislav_chumak@mail.ru>
2023-04-07 00:16:02 +0300

    update project

---
+++ src/main/java/searchengine/config/UserStopIndexingException.java
@@ -0,0 +1,9 @@
+package searchengine.config;
+
+public class UserStopIndexingException extends Exception {
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+
+}
